bfa419d969fdc8707207d5157a0ee6230cd86bb9
Elchristo-j <hiroshi.77dk@gmail.com>
2026-05-17 18:24:16 +0900

    DB設定とCSRF修正

--- config/settings.py
+++ config/settings.py
@@ -146,11 +146,23 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # データベース（DATABASE_URL環境変数があればPostgreSQLを使う）
+import dj_database_url
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.up.railway.app',
     'https://itadaki.el-christo.online',
+    'https://toripon.el-christo.online',
 ]
