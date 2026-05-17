da71531e439c13f07ddef542d5632432d5117982
Elchristo-j <hiroshi.77dk@gmail.com>
2026-05-17 22:39:05 +0900

    Update create_admin.py

--- reservations/management/commands/create_admin.py
+++ reservations/management/commands/create_admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 class Command(BaseCommand):
     def handle(self, *args, **options):
         User = get_user_model()
-        if not User.objects.filter(username='admin').exists():
+        if not User.objects.filter(username='Chief_Administrator').exists():
             User.objects.create_superuser('admin', 'admin@example.com', 'itadaki2026')
             self.stdout.write('Superuser created')
         else:
