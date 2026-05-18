1b60e1ba0759717561ffc0ccd30c016284ef1857
Elchristo-j <hiroshi.77dk@gmail.com>
2026-05-18 22:47:09 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from orders.views import top
 
 urlpatterns = [
+    path('', top, name='top'),  # ← これを追加
+    # 既存のパスはそのまま
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
