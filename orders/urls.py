3fc21da4336af0eac43065ac68430ec30ac9f56b
Elchristo-j <hiroshi.77dk@gmail.com>
2026-05-19 08:21:29 +0900

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('staff/merge/', views.staff_order_merge, name='staff_order_merge'),
     path('staff/close/', views.staff_order_close, name='staff_order_close'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('produce/staff/', views.produce_staff_list, name='produce_staff_list'),
+    path('produce/staff/action/', views.produce_staff_action, name='produce_staff_action'),
 ]
