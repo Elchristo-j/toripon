7e4a072bfcc4277c8b798239efabc2474e1b6de5
Elchristo-j <hiroshi.77dk@gmail.com>
2026-05-19 08:12:40 +0900

    Update store_urls.py

--- orders/store_urls.py
+++ orders/store_urls.py
@@ -5,4 +5,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('table/<str:seat_code>/', views.order_menu, name='menu'),
+    path('produce/', views.produce_order_form, name='produce_form'),
+    path('produce/order/', views.produce_order_submit, name='produce_submit'),
 ]
