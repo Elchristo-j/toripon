c0ab91423f0e4fc119a983f8134310ca84ea7ba6
Elchristo-j <hiroshi.77dk@gmail.com>
2026-05-19 22:39:07 +0900

    Add delivery URLs to store_urls

--- orders/store_urls.py
+++ orders/store_urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('table/<str:seat_code>/', views.order_menu, name='menu'),
     path('produce/', views.produce_order_form, name='produce_form'),
     path('produce/order/', views.produce_order_submit, name='produce_submit'),
+    path('delivery/', views.delivery_order_form, name='delivery_form'),
+    path('delivery/order/', views.delivery_order_submit, name='delivery_submit'),
 ]
